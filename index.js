32df5cd4aafb98b2321b59fc51aadc54a956f654
Theepan Kanthavel <jatheepan@bibliocommons.com>
2018-07-24 17:04:09 -0400

    CRUD functionality

--- index.js
+++ index.js
@@ -12,13 +12,29 @@ if(!cmd) {
 
 switch(cmd) {
   case 'list':
-    console.table(store.list());
+    const list = store.list();
+    console.table(list.items);
+    console.log('Total:', list.count);
     break;
+
   case 'save':
-    store.save();
+    const item = store.save({Title: args.title});
+    console.log(item.Title, 'is saved');
     break;
+
+  case 'update':
+    const updatedItem = store.update(args.id, args.title);
+    console.log( updatedItem.Title, 'is updated.' );
+    break;
+
   case 'delete':
-    store.delete(args.id);
+    const deletedItem = store.delete(args.id);
+    console.log( deletedItem.Title, 'is deleted.' );
+    break;
+
+  case 'clear':
+    store.deleteAll();
+    break;
   default:
     console.log('Not a valid command');
 }
--- store.js
+++ store.js
@@ -1,18 +1,69 @@
 const LocalStorage = (require( 'node-localstorage' )).LocalStorage;
 const localStorage = new LocalStorage( './db' );
+const KEY = 'items';
+
+LocalStorage.prototype.__set__ = function( key, value ) {
+  const items = JSON.stringify( value );
+  this.setItem( key, items );
+};
+LocalStorage.prototype.__get__ = function( key ) {
+  return JSON.parse( this.getItem( key ) );
+};
+
+const getCollection = () => {
+  const items = localStorage.getItem( KEY );
+  if(!items) {
+    localStorage.setItem('items', JSON.stringify([]));
+  }
+  return localStorage;
+};
 
 module.exports = {
-  save() {
-    return [];
+  _store: getCollection(),
+
+  save(item) {
+    let items = this._store.__get__( KEY );
+    items.unshift(item);
+    this._store.__set__( KEY, items );
+    return item;
   },
+
   list() {
-    return [];
+    let items = this._store.__get__(  KEY );
+    items = items.map((item, index) => ({
+      Id: index + 1,
+      Title: item.Title
+    }));
+    return {
+      count: items.length,
+      items
+    };
   },
-  delete() {
-    return null;
+
+  delete( id ) {
+    const index = id - 1;
+    let items = this._store.__get__( KEY );
+    let removedItem = items.shift( index, 1 );
+    this._store.__set__( 'items', items );
+
+    return removedItem;
   },
-  update() {
-    return null;
+
+  update( id, value ) {
+    const index = id - 1;
+    const items = this._store.__get__( KEY );
+
+    let item = items[index];
+    if( !item || !value ) {
+      return null;
+    }
+    item.Title = value;
+    this._store.__set__( KEY, items );
+    return item;
+  },
+
+  deleteAll() {
+    this._store.setItem('items', JSON.stringify([]));
   }
 }
 
